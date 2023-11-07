8d25df382ddc46f83c031610d8599c4bef286034
mugdharvu <suresh.mugdha@gmail.com>
2023-11-07 09:21:56 +0530

    Yo

---
+++ sum.py
@@ -0,0 +1,12 @@
+def add_numbers(number1, number2):
+    """
+    Returns the sum of two numbers.
+
+    Args:
+    number1 (int): The first number.
+    number2 (int): The second number.
+
+    Returns:
+    int: The sum of number1 and number2.
+    """
+    return number1 + number2
